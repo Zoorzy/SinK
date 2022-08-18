80c1d7c65ba95c206f0134f37dcdde81703c1b97
Zoorzy <64961650+Zoorzy@users.noreply.github.com>
2022-08-18 15:02:58 +0200

    .

--- public/js/workersHandler.js
+++ public/js/workersHandler.js
@@ -14,21 +14,24 @@
   }
   function workerDone(e) {
     --running;
-    var newprogress = "15"
+    var newprogress = "20"
 
+    // Scan Code Button
     var scanBtn = document.getElementsByClassName('scanBtn')[e.data.id]
     scanBtn.disabled = false
     scanBtn.textContent = "Scan"
 
+    // View Code Button
     var viewBtn = document.getElementsByClassName('viewBtn')[e.data.id]
     viewBtn.disabled = false
 
+    // Download Code Button 
     var downloadBtn = document.getElementsByClassName('downloadBtn')[e.data.id]
     downloadBtn.disabled = false
 
+    // Progress Bar
     var progressBar = document.getElementsByClassName('scanProgressBar')[e.data.id]
     progressBar.ariaValueNow = newprogress
     progressBar.style.width = newprogress + "%"
-    //$(progressBar).attr('aria-valuenow', newprogress).css('width', newprogress+"%");
   }
 })();
