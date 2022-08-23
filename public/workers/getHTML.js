4ece91a4d816a562274456c6645e8843cc832c6e
Zoorzy <64961650+Zoorzy@users.noreply.github.com>
2022-08-23 18:54:36 +0200

    todo

--- public/workers/getHTML.js
+++ public/workers/getHTML.js
@@ -4,14 +4,9 @@ this.onmessage = e => {
   var params = 'url=' + e.data.url
   xhttp.open('POST', url, true)
 
-  //Send the proper header information along with the request
   xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-
   xhttp.onreadystatechange = function () {
-
-    if (xhttp.readyState == 4 && xhttp.status) {
-      self.postMessage({ id: e.data.id, status: xhttp.status, html: xhttp.responseText })
-    }
+    if (xhttp.readyState == 4 && xhttp.status) self.postMessage({ id: e.data.id, status: xhttp.status, html: xhttp.responseText })
   }
   xhttp.send(params)
 }
