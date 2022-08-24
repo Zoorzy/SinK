8b644a4c33943f58a7416e3e58ae96bb88c37fd4
Zoorzy <64961650+Zoorzy@users.noreply.github.com>
2022-08-24 16:55:30 +0200

    recover external js file content

--- public/js/scanHTML.js
+++ public/js/scanHTML.js
@@ -1,6 +1,26 @@
 function scanHTML(sources, sinks) {
   var textarea = document.getElementById('txthtml').value
-  console.log(textarea)
-  console.log(sources)
-  console.log(sinks)
+  getExternalJs(textarea)
+}
+function getExternalJs(txtString) {
+  //parse string to html
+  var html = new DOMParser().parseFromString(txtString, "text/xml");
+  //look for <script src="">
+  var scripts = html.getElementsByTagName('script'), srcUrl, worker, base = document.getElementById('url').value
+  console.log('base: ' + base)
+  Array.prototype.slice.call(scripts).forEach(element => {
+    if (element.hasAttribute('src')) {
+      //stucture a correct url
+      srcUrl = new URL(element.getAttribute('src'), base)
+      //download from url
+      worker = new Worker("../../public/workers/getHTML.js")
+      worker.onmessage = workerDone
+      worker.postMessage({ url: String(srcUrl) })
+    }
+  })
+  function workerDone(e) {
+    console.log(e.data.html)
+    //concat with original html
+    
+  }
 }
