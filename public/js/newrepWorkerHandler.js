5d5e0f40ede906498321d97eae15207cd9d8ebc6
Zoorzy <64961650+Zoorzy@users.noreply.github.com>
2022-08-24 14:27:27 +0200

    before scanHTML static analysis

--- public/js/newrepWorkerHandler.js
+++ public/js/newrepWorkerHandler.js
@@ -3,14 +3,37 @@
   if (document.getElementById('url')) url = document.getElementById('url').value
 
   if (typeof url !== 'undefined') {
-    worker = new Worker("../../public/workers/getHTML.js");
-    worker.onmessage = workerDone;
-    worker.postMessage({ url: url });
+    worker = new Worker("../../public/workers/getHTML.js")
+    worker.onmessage = workerDone
+    worker.postMessage({ url: url })
   }
   function workerDone(e) {
     document.getElementById('txthtml').textContent = e.data.html
     document.getElementById('copyBtn').disabled = false
     document.getElementById('downloadBtn').disabled = false
     document.getElementById('viewBtn').disabled = false
+    getReadyToScan()
+  }
+  function getReadyToScan() {
+    sources = ['document.location.href',
+      'document.location',
+      'window.location.href',
+      'window.location']
+    sinks = ['$X.insertAdjacentHTML(...)',
+      '$X.innerHTML(...)',
+      '$X.innerHTML = ...']
+    var patternSources = document.getElementById('pattern-sources')
+    for (var i = 0; i < sources.length; i++) {
+      var dd = document.createElement('dd')
+      dd.textContent = sources[i]
+      patternSources.appendChild(dd)
+    }
+    var patternSinks = document.getElementById('pattern-sinks')
+    for (var i = 0; i < sinks.length; i++) {
+      var dd = document.createElement('dd')
+      dd.textContent = sinks[i]
+      patternSinks.appendChild(dd)
+    }
+    scanHTML(sources, sinks)
   }
 })();
---
+++ public/js/scanHTML.js
@@ -0,0 +1,6 @@
+function scanHTML(sources, sinks) {
+  var textarea = document.getElementById('txthtml').value
+  console.log(textarea)
+  console.log(sources)
+  console.log(sinks)
+}
